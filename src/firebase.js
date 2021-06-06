fdd6fe6eadf7bd8390f48d5ae6839734467a1776
klmnankit1511 <klmnankit1511@gmail.com>
2021-06-06 23:50:31 +0530

    Upload File

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBdZkwI1CcNC2UwiANLm_qlzZ4ruKkV8mc",
+  authDomain: "test-d6bec.firebaseapp.com",
+  projectId: "test-d6bec",
+  storageBucket: "test-d6bec.appspot.com",
+  messagingSenderId: "101394890413",
+  appId: "1:101394890413:web:d102ec4fe23034b191d024",
+  measurementId: "G-NF89RWSQGL",
+};
