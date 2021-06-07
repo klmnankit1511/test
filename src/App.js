0831a447d59dd66ee70cb53cf218c63c3578b89e
klmnankit1511 <klmnankit1511@gmail.com>
2021-06-07 19:30:51 +0530

    Merge

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello hj</h1>
-      <h2>Start fs editing to see s .  kjashome magic happen!</h2>
+      <h2>Start fs ehello new branchkjashome magic happen!</h2>
     </div>
   );
 }
